194a59fbf262dc68be9c172e985652a678441da7
Marius Sørenes <marsor354@gmail.com>
2024-11-25 15:31:14 +0100

    changed redirect url

--- marius-tanker-frontend/app/service/auth.server.ts
+++ marius-tanker-frontend/app/service/auth.server.ts
@@ -6,10 +6,7 @@ const gitHubStrategy = new GitHubStrategy(
   {
     clientId: process.env.GITHUB_AUTH_CLIENT_ID ?? "",
     clientSecret: process.env.GITHUB_AUTH_SECRET ?? "",
-    redirectURI:
-      process.env.NODE_ENV === "development"
-        ? "http://localhost:5173/auth/github/callback"
-        : "https://marius-tanker.no/auth/github/callback",
+    redirectURI: "https://marius-tanker.no/auth/github/callback",
   },
   async ({ accessToken, extraParams, profile }) => {
     return profile;
