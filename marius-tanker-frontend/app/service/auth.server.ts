78b85349229ded693a42321bfcf85fe811eb455a
Marius Sørenes <marsor354@gmail.com>
2024-11-26 08:49:41 +0100

    wip

--- marius-tanker-frontend/app/service/auth.server.ts
+++ marius-tanker-frontend/app/service/auth.server.ts
@@ -2,6 +2,8 @@ import { GitHubStrategy } from "remix-auth-github";
 import { Authenticator } from "remix-auth";
 import { sessionStorage } from "./session.server";
 
+console.log(process.env.GITHUB_AUTH_CLIENT_ID, process.env.GITHUB_AUTH_SECRET);
+
 const gitHubStrategy = new GitHubStrategy(
   {
     clientId: process.env.GITHUB_AUTH_CLIENT_ID ?? "",
@@ -9,9 +11,12 @@ const gitHubStrategy = new GitHubStrategy(
     redirectURI: "https://marius-tanker.no/auth/github/callback",
   },
   async ({ accessToken, extraParams, profile }) => {
+    console.log(accessToken, extraParams, profile);
     return profile;
   }
 );
+
+console.error(gitHubStrategy);
 
 export const authenticator = new Authenticator(sessionStorage);
 authenticator.use(gitHubStrategy);
